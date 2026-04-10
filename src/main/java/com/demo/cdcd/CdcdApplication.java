fa911512971b371c7f972842deb763633d849ced
kdp-alpha <kdpawar752@gmail.com>
2026-04-10 12:19:28 +0530

    code commited

---
+++ src/main/java/com/demo/cdcd/CdcdApplication.java
@@ -0,0 +1,21 @@
+package com.demo.cdcd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class CdcdApplication {
+
+	@GetMapping("/welcome")
+	public String welcome() {
+		return "Welcome to CI CD Application!";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(CdcdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/cdcd/CdcdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.cdcd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CdcdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
