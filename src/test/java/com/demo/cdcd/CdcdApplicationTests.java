f7e2776f478ac008a6d6aeae4dfa33abea3a87f9
kdp-alpha <kdpawar752@gmail.com>
2026-04-10 16:59:46 +0530

    test cases added

--- src/test/java/com/demo/cdcd/CdcdApplicationTests.java
+++ src/test/java/com/demo/cdcd/CdcdApplicationTests.java
@@ -3,11 +3,19 @@ package com.demo.cdcd;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 class CdcdApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void welcomeMessageShouldMatch() {
+		CdcdApplication app = new CdcdApplication();
+		assertEquals("Welcome to CI CD Application", app.welcome());
 	}
 
 }
